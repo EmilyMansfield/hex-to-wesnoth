2fcb003860c86d760fb188c2d48b25941bc5b49c
Daniel Mansfield <daniel.mansfield99@gmail.com>
2014-01-08 17:26:54 +0000

    Overhauled system to compare images to an archive instead of blurring the image and checking pixels. Method has upsides and downsides, currently does not function correctly
    Signed-off-by: Daniel Mansfield <daniel.mansfield99@gmail.com>

--- main.cpp
+++ main.cpp
@@ -21,85 +21,7 @@
 #include <fstream>
 #include <string>
 #include <cmath>
-
-class Tile
-{
-	public:
-
-	sf::Color color;
-	std::string name;
-
-	Tile(sf::Color color, std::string name)
-	{
-		this->color = color;
-		this->name = name;
-	}
-
-	bool equal(sf::Color color)
-	{
-		unsigned int t = 1;
-
-		return (this->color.r - t <= color.r && this->color.r + t >= color.r) &&
-			(this->color.g - t <= color.g && this->color.g + t >= color.g) &&
-			(this->color.b - t <= color.b && this->color.b + t >= color.b) &&
-			(this->color.a - t <= color.a && this->color.a + t >= color.a);
-	}
-};
-
-// Linearly blur the image with the specified radius in one dimension,
-// then return its transpose. When called twice this efficiently blurs
-// the image in two dimensions
-sf::Image blur_1d(sf::Image& img, unsigned int r)
-{
-	sf::Image result;
-	result.create(img.getSize().y, img.getSize().x);
-
-	// Iterate over each pixel and blur it horizontally
-	for(unsigned int y = 0; y < img.getSize().y; ++y)
-	{
-		for(unsigned int x = 0; x < img.getSize().x; ++x)
-		{
-			// Running sum of pixel values (RGB)
-			// Don't use sf::Color as we will easily exceed char bounds
-			unsigned int pixelSum[3] = { 0, 0, 0 };
-			// Number of pixels tested (Used to compute average)
-			unsigned int n = 0;
-
-			for(int x2 = (int)(x-r); x2 <= (int)(x+r); ++x2)
-			{
-				// Skip if out of bounds
-				if(x2 < 0 || x2 >= img.getSize().x)
-					continue;
-
-				// Add pixel to running sum
-				sf::Color pixelCol = img.getPixel(x2, y);
-				pixelSum[0] += pixelCol.r;
-				pixelSum[1] += pixelCol.g;
-				pixelSum[2] += pixelCol.b;
-				++n;
-			}
-
-			// Save blurred pixel value to transposed position in the
-			// result image
-			if(n == 0) n = 1;
-			sf::Color pixelCol(pixelSum[0] / n, pixelSum[1] / n, pixelSum[2] / n);
-			result.setPixel(y, x, pixelCol);
-		}
-	}
-
-	return result;
-}
-
-// Linearly blur the image with the specified radius. Slow, but more
-// useful than gaussian in this case as we can limit the radius exactly.
-sf::Image blur(sf::Image& img, unsigned int r)
-{
-	// Blur horizontally
-	sf::Image tmp = blur_1d(img, r);
-
-	// Blur vertically
-	return blur_1d(tmp, r);
-}
+#include <utility>
 
 void printHelp()
 {
@@ -110,13 +32,37 @@ void printHelp()
 	std::cerr << "  -t\tspecify tile width and height. Default 32 34\n";
 	std::cerr << "  -i\tspecify initial tile offset. Default 26 23\n";
 	std::cerr << "  -m\ttop-left tile is a major (upper) tile not a minor\n";
-	std::cerr << "  -b\tspecify blur radius. Default prefiltered\n";
 	std::cerr << "  -h\tshow this help and exit\n\n";
 	std::cerr << "Examples:\n";
 	std::cerr << "  hex-to-wesnoth map.png -x 16 17 -o output.map\t";
 	std::cerr << "Convert map.png to output.map with tile width 16 and height 17\n";
 
 	return;
+}
+
+// Return true if the images are equal, only comparing non-transparent
+// pixels in a to those in b
+bool image_compare(sf::Image& a, sf::Image& b)
+{
+	// If the images are not the same size then return false, they
+	// cannot be equal to each other
+	if(a.getSize() != b.getSize()) return false;
+
+	for(unsigned int y = 0; y < a.getSize().y; ++y)
+	{
+		for(unsigned int x = 0; x < a.getSize().x; ++x)
+		{
+			// Ignore non-opaque alpha values
+			if(a.getPixel(x, y).a != 0xff) continue;
+			// Compare rgb, return false if discrepancy
+			if( a.getPixel(x, y).r != b.getPixel(x, y).r ||
+				a.getPixel(x, y).g != b.getPixel(x, y).g ||
+				a.getPixel(x, y).b != b.getPixel(x, y).b) return false;
+		}
+	}
+
+	// No discrepancies so images are equal
+	return true;
 }
 
 int main(int argc, char *argv[])
@@ -132,9 +78,6 @@ int main(int argc, char *argv[])
 
 	unsigned int initialOffsetX = 26;
 	unsigned int initialOffsetY = 23;
-
-	// Radius of the blur to apply. 0 => pre-filtered image
-	unsigned int blurRadius = 0;
 
 	// First tile is an upper one and not a lower (Wesnoth requires lower)
 	bool majorTileStart = false;
@@ -190,16 +133,6 @@ int main(int argc, char *argv[])
 				initialOffsetY = std::stoi(argv[i+2]);
 				i += 2;
 			}
-			// Blur radius
-			else if((std::string)argv[i] == "-b")
-			{
-				if(i+1 >= argc || argv[i+1][0] == '-')
-				{
-					std::cerr << "-b requires an argument\n";
-					return 1;
-				}
-				blurRadius = std::stoi(argv[++i]);
-			}
 			// Top left tile is a major one (Wesnoth requires minor)
 			else if((std::string)argv[i] == "-m")
 			{
@@ -239,63 +172,24 @@ int main(int argc, char *argv[])
 	// Open the data file
 	std::ifstream dataFile(dataFileName, std::ifstream::in);
 
-	// Map of colours to their respective tile names
-	// Not using actual map or unordered_map because then we have to
-	// go to the trouble of implementing hash functions, which is
-	// completely pointless due to the small size of the data set
-	// (which will always be small)
-	std::vector<Tile> tileMap;
+	// Vector of tile images and their respective tile names
+	std::vector<std::pair<sf::Image, std::string>> tileMap;
 
-	// Data file consists of 3-byte hex number followed by a string
-	// (space separated). Number equates to centre pixel RGB value
-	// of the blurred tile blurred with a radius equal to the minimum
-	// distance from the centre to an edge
+	// Data file consists of tile filename and tile name for use in
+	// the final map file, separated by a space. Filenames should be
+	// (though not a necessity) local to the executable directory
 	while(dataFile.good())
 	{
-		unsigned int pixel = 0x000000;
+		std::string filename = "data/placeholder.png";
 		std::string tileString = "Gg";
 
-		dataFile >> std::hex >> pixel >> tileString;
+		dataFile >> filename >> tileString;
 
-		// Convert RGB value to an SFML colour
-		sf::Color pixelCol;
-		pixelCol.r = (pixel & 0xFF0000) >> 16;
-		pixelCol.g = (pixel & 0x00FF00) >> 8;
-		pixelCol.b =  pixel & 0x0000FF;
-
-		// Create the tile and add it to the tile map
-		tileMap.push_back(Tile(pixelCol, tileString));
+		// Load the image and add it to the tile map
+		sf::Image tileImage;
+		tileImage.loadFromFile(filename);
+		tileMap.push_back(std::make_pair(tileImage, tileString));
 	}
-	//~ Tile tileMap[] =
-	//~ {
-		//~ Tile(sf::Color(0x71, 0x93, 0xbf), "Wog"),	// Ocean
-		//~ Tile(sf::Color(0xf2, 0xf2, 0xf2), "Aa"),	// Snowy plains
-		//~ Tile(sf::Color(0xbe, 0xbe, 0xbe), "Aa^Fpa"),// Snowy deciduous forest
-		//~ Tile(sf::Color(0xea, 0xea, 0xea), "Ha"),	// Snowy hills
-		//~ Tile(sf::Color(0xc6, 0xc6, 0xc6), "Ms"),	// Snowy mountains
-		//~ Tile(sf::Color(0x79, 0x79, 0x79), "Ms^Xm"),	// Snowy tall mountains
-		//~ Tile(sf::Color(0xb9, 0xe0, 0x7e), "Gll"),	// Green plains
-		//~ Tile(sf::Color(0xa7, 0xce, 0x6c), "Gll^Gvs"), // Grass
-		//~ Tile(sf::Color(0x96, 0xb8, 0x8f), "Gll^Fmw"), // Grassy forest
-		//~ Tile(sf::Color(0xb7, 0xd6, 0x52), "Hh"), // Grassy hills
-		//~ Tile(sf::Color(0xdb, 0x90, 0x72), "Mm"), // Mountain
-		//~ Tile(sf::Color(0x98, 0xbb, 0xaa), "Ss"), // Swamp
-		//~ Tile(sf::Color(0x93, 0xb8, 0xaf), "Sm"), // Marsh
-		//~ Tile(sf::Color(0xcd, 0xcd, 0xcd), "Rb"), // Dirt
-		//~ Tile(sf::Color(0x9f, 0x9f, 0x9f), "Aa^Fetd"), // Dead tree
-		//~ Tile(sf::Color(0x88, 0x88, 0x88), "Cha"), // Snowy castle
-		//~ Tile(sf::Color(0x83, 0xaa, 0x48), "Ch"), // Castle
-		//~ Tile(sf::Color(0x76, 0x9d, 0x3c), "Kv"), // Temple
-		//~ Tile(sf::Color(0x6f, 0x6f, 0x6f), "Kv"), // Snowy temple
-		//~ Tile(sf::Color(0x84, 0x84, 0x84), "Kea"), // Snowy mine
-		//~ Tile(sf::Color(0x6e, 0x6e, 0x63), "Kha"), // Snowy tower
-		//~ Tile(sf::Color(0x81, 0xa8, 0x46), "Ke"), // Mine
-		//~ Tile(sf::Color(0x76, 0x9d, 0x3b), "Kh"), // Tower
-		//~ Tile(sf::Color(0x96, 0xbd, 0x5b), "Qxu"), // Cave
-		//~ Tile(sf::Color(0xaa, 0xaa, 0xaa), "Khr"), // Dungeon
-		//~ Tile(sf::Color(0x73, 0x73, 0x73), "Aa^Vha"), // Snowy village
-		//~ Tile(sf::Color(0xa4, 0xa4, 0xa4), "Aa^Vhca") // Snowy city
-	//~ };
 
 	// Default tile if there is no match in the tile map
 	std::string defaultTile = "Gg";
@@ -303,46 +197,16 @@ int main(int argc, char *argv[])
 	// Output the map header to the map file
 	outFile << "border_size=1\n" << "usage=map\n" << std::endl;
 
-	// Assume the image has been filtered correctly already if blurRadius
-	// is 0 (or not specified), otherwise blur the input file and
-	// lazily implement Gaussian blur by performing the blur m times with
-	// a radius 1/m of the specified blur radius
-	sf::Image result;
-	unsigned int iterations = 3; // Brightness multiplier depends on iterations!
-	double bMult = 1.016;
-	if(iterations > blurRadius) iterations = blurRadius;
-	if(blurRadius == 0) result = inFile;
-	else
-	{
-		result = blur(inFile, blurRadius / iterations);
-
-		for(unsigned int i = iterations; i > 1; --i)
-		{
-			result = blur(result, blurRadius / iterations);
-		}
-		// Apply the brightness modifier to more closely match a Gaussian
-		// It might have simply been easier to just use a Gaussian in the
-		// first place
-		for(unsigned int y = 0; y < result.getSize().y; ++y)
-		{
-			for(unsigned int x = 0; x < result.getSize().x; ++x)
-			{
-				sf::Color col = result.getPixel(x, y);
-				col.r *= bMult;
-				col.g *= bMult;
-				col.b *= bMult;
-				result.setPixel(x, y, col);
-			}
-		}
-	}
-
-	// Iterate through the centre points of each tile and use the colour
-	// value at the point to determine which tile should be added, before
-	// outputting the tile to the map file
+	// Iterate over the file in step sizes equal to the tile dimensions
+	// and compare each pixel that is not transparent (alpha = 0) in that
+	// square to each tile in turn. If all pixels are equal, then the tiles
+	// match and the terrain code is outputted to the file. Otherwise move
+	// on to the next tile
 	for(unsigned int y = initialOffsetY; y < inFile.getSize().y; y += tileOffsetY)
 	{
 		for(unsigned int x = initialOffsetX; x < inFile.getSize().x; x += tileOffsetX)
 		{
+			// Adjust for hexagonal minor/major tiling
 			unsigned int yTmp = y;
 			if(((x - initialOffsetX) / tileOffsetX) % 2 == 0)
 			{
@@ -351,21 +215,22 @@ int main(int argc, char *argv[])
 			if(x != initialOffsetX)
 			{
 				outFile << ", ";
-				//~ std::cout << ", ";
 			}
-	
-			// If the tileMap contains a tile for the colour of the pixel,
-			// then add that tile to the map, otherwise add a default tile
+
+			// Create the small subimage to compare to
+			sf::Image subimage;
+			subimage.copy(inFile, 0, 0, sf::IntRect(x, y, tileOffsetX, tileOffsetY));
+
+			// Iterate over every tile in the tile map and compare to the
+			// subimage
 			bool found = false;
-			sf::Color col = result.getPixel(x, yTmp);
-			//~ std::cout << std::hex << (int)col.r << " " << (int)col.g << " " << (int)col.b;
 			for(auto tile : tileMap)
 			{
-				if(tile.equal(col))
+				// If the subimage matches one of the tiles then output
+				// the corresponding name
+				if(found = image_compare(tile.first, subimage))
 				{
-					outFile << tile.name;
-					found = true;
-					break;
+					outFile << tile.second;
 				}
 			}
 			if(!found) outFile << defaultTile;
